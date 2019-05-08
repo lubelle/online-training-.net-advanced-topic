d3a0d2ee75a5e744178ad44af0724b496264844d
lubelle <belleliullh@gmail.com>
2019-05-08 19:19:09 -0400

    use delegate to achieve open close principle

---
+++ DelegatesDemo/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DelegatesDemo
+{
+    public class PhotoProcessor
+    {
+        public void Process(string path)
+        {
+            var photo = Photo.Load(path);
+
+            var filters = new PhotoFilters();
+            filters.ApplyBrightness(photo);
+            filters.ApplyContrast(photo);
+            filters.Resize(photo);
+
+            photo.Save();
+        }
+    }
+
+    public class Photo
+    {
+        public static Photo Load(string path)
+        {
+            return new Photo();
+        }
+
+        public void Save()
+        {
+            Console.WriteLine("Persist the photo to database ..");
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var photoProcessor = new PhotoProcessor();
+            photoProcessor.Process("C:\\abc.jpg");
+
+
+            Console.ReadLine();
+        }
+    }
+
+    class PhotoFilters
+    {
+        public void ApplyBrightness(Photo photo)
+        {
+            Console.WriteLine("Apply brightness...");
+        }
+        public void ApplyContrast(Photo photo)
+        {
+            Console.WriteLine("Apply contrast...");
+        }
+        public void Resize(Photo photo)
+        {
+            Console.WriteLine("Apply resize...");
+        }
+    }
+}
